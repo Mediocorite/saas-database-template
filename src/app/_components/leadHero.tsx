75eeb2d3bf3fb7defe9b20f39e2fe1ba5a0301e6
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-03-08 21:20:53 +0000

    Customer stories done

--- src/app/_components/leadHero.tsx
+++ src/app/_components/leadHero.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -12,19 +13,25 @@ type ButtonVariant =
   | null
   | undefined;
 
-// Define the structure of objects within heroButtons
 interface HeroButton {
   id: number;
   name: string;
   route: string;
-  variant: ButtonVariant; // Use the ButtonVariant type for variant
+  variant: ButtonVariant;
 }
 
-// Define the structure of the config object
+interface CustomerBranding {
+  id: number;
+  src: string;
+  alt: string;
+}
+
 interface Config {
   heroTitle: string;
   heroDescription: string;
   heroButtons: HeroButton[];
+  customerStories: string;
+  customerBranding: CustomerBranding[];
 }
 
 const config: Config = {
@@ -45,25 +52,58 @@ const config: Config = {
       variant: "outline",
     },
   ],
+  customerStories:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid mollitia ipsum cupiditate magnam harum voluptatum at explicabo sint ut.",
+  customerBranding: [
+    { id: 1, src: "/images/fake-brand-logo.png", alt: "fakeBrandName" },
+    { id: 2, src: "/images/fake-brand-logo.png", alt: "fakeBrandName" },
+    { id: 3, src: "/images/fake-brand-logo.png", alt: "fakeBrandName" },
+    { id: 4, src: "/images/fake-brand-logo.png", alt: "fakeBrandName" },
+    { id: 5, src: "/images/fake-brand-logo.png", alt: "fakeBrandName" },
+  ],
 };
 
 export default function LeadHero() {
   return (
-    <section className="flex  flex-col items-center justify-center px-40 pt-28 text-center leading-tight">
-      <h1 className="text-7xl font-bold">{config.heroTitle}</h1>
-      <span className="mb-7 mt-3 px-7 text-lg">{config.heroDescription}</span>
-      <div className="flex gap-7 p-3">
-        {config.heroButtons.map((element) => (
-          <Button
-            key={element.id}
-            size={"lg"}
-            asChild
-            variant={element.variant}
-          >
-            <Link href={element.route}>{element.name}</Link>
-          </Button>
-        ))}
-      </div>
-    </section>
+    <>
+      <section className="flex  flex-col items-center justify-center px-40 pt-28 text-center leading-tight">
+        <h1 className="text-7xl font-bold">{config.heroTitle}</h1>
+        <span className="mb-7 mt-3 px-7 text-lg">{config.heroDescription}</span>
+        <div className="flex gap-7 p-3">
+          {config.heroButtons.map((element) => (
+            <Button
+              key={element.id}
+              size={"lg"}
+              asChild
+              variant={element.variant}
+            >
+              <Link href={element.route}>{element.name}</Link>
+            </Button>
+          ))}
+        </div>
+        <div className="flex items-center justify-center">
+          <iframe
+            src="./images/leadVideo.html"
+            height={400}
+            width={750}
+          ></iframe>
+        </div>
+        <div className="my-10 flex items-center justify-center text-center text-lg font-semibold">
+          {config.customerStories}
+        </div>
+        <div className="item-center mx-10 flex justify-between gap-7">
+          {config.customerBranding.map((element) => (
+            <Image
+              key={element.id}
+              src={element.src}
+              alt={element.alt}
+              width={100}
+              height={100}
+            />
+          ))}
+        </div>
+        <div className="text-blue-500">{`Read our customer stories ->`}</div>
+      </section>
+    </>
   );
 }
